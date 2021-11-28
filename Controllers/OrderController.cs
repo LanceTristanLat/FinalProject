7f56a736b5c7007e142a11fbcceec9a890348c03
abi magpatoc <abimgptc@gmail.com>
2021-11-28 22:09:44 +0800

    order controller and model added

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var list = _context.Orders.ToList();
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        //public IActionResult Create(Order record)
+        //{
+        //    var order = new Order();
+        //    order.ProductName = record.ProductName;
+        //    order.Quantity = record.Quatity;
+        //    order.Address = record.Address;
+        //    order.ContactNum = record.ContactNum;
+        //    order.ModeOfDelivery = record.ModeOfDelivery;
+
+        //    _context.Orders.Add(order);
+        //    _context.SaveChanges();
+
+        //    return RedirectToAction("Index");
+        //}
+
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    var product = _context.Items.Where(p => p.OrderID == id).SingleOrDefault();
+        //    if (product == null)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(product);
+        //}
+        [HttpPost]
+        //public IActionResult Edit(int? id, Order record)
+        //{
+        //    var order = _context.Orders.Where(i => i.OrderID == id).SingleOrDefault();
+        //    var order = new Order();
+        //    order.ProductName = record.ProductName;
+        //    order.Quantity = record.Quatity;
+        //    order.Address = record.Address;
+        //    order.ContactNum = record.ContactNum;
+        //    order.ModeOfDelivery = record.ModeOfDelivery;
+
+        //    _context.Orders.Update(order);
+        //    _context.SaveChanges();
+
+        //    return RedirectToAction("Index");
+        //}
+        //public IActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    var order = _context.Orders.Where(i => i.OrderID == id).SingleOrDefault();
+        //    if (order == null)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    _context.Orders.Remove(order);
+        //    _context.SaveChanges();
+
+        //    return RedirectToAction("Index");
+        //}
+
+        //public IActionResult Receipt()
+        //{
+        //    var list = _context.Orders.ToList();
+        //    return View(list);
+        //}
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }
+        [Required(ErrorMessage = "Required.")]
+        [Display(Name = "Product Name")]
+
+        public string ProductName { get; set; }
+
+
+        [Required(ErrorMessage = "Required.")] 
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Required.")] 
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        [DataType(DataType.MultilineText)]
+
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Required.")] 
+        public int ContactNum { get; set; }
+
+        [Display(Name = "Date Ordered")]
+        public DateTime DateAdded { get; set; }
+
+        public ModeOfPayment ModeOfPayment{ get; set; }
+        
+    }
+
+    public enum ModeOfPayment
+    {
+        COD = 1,
+        EPayment = 2,
+        BankTransfer = 3
+    }
+}
